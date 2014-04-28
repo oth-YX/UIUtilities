c642c4f0f4d3ecbe8fb0eb97fdce578acf723f4d
oth-YX <oth-YX@users.noreply.github.com>
2014-04-28 18:51:13 +0300

    Create UIView+ColoredBorderAroundComponents.h

---
+++ UIView+ColoredBorderAroundComponents.h
@@ -0,0 +1,13 @@
+//
+//  UIView+ColoredBorderAroundComponents.h
+//  YlockTest
+//
+//  Created by oth on 4/13/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (ColoredBorderAroundComponents)
+
+@end
