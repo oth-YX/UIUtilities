5378761d8bab1c0453b76bdfac385ef3c900ae86
oth-YX <oth-YX@users.noreply.github.com>
2014-05-19 15:43:50 +0300

    Update Utilities.m

--- Utilities.m
+++ Utilities.m
@@ -1,9 +1,9 @@
 //
 //  Utilities.m
-//  evernote-sdk-ios
+//  
 //
-//  Created by A1@YX on 5/19/14.
-//  Copyright (c) 2014 n/a. All rights reserved.
+//  Created by oth on 5/19/14.
+//
 //
 
 #import "Utilities.h"
@@ -13,11 +13,12 @@
 
 //This function returns a UIImage after cutom resizing
 + (UIImage *)imageWithImage:(UIImage *)image scaledToSize:(CGSize)newSize {
-    //UIGraphicsBeginImageContext(newSize);
+    
     UIGraphicsBeginImageContextWithOptions(newSize, NO, 0.0);
     [image drawInRect:CGRectMake(0, 0, newSize.width, newSize.height)];
     UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
     UIGraphicsEndImageContext();
+    
     return newImage;
 }
 @end
