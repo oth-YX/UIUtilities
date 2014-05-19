e7425643f3b21ae4a0647f84da865837132dc601
oth-YX <oth-YX@users.noreply.github.com>
2014-05-19 15:40:55 +0300

    Create Utilities.h

---
+++ Utilities.h
@@ -0,0 +1,14 @@
+//
+//  Utilities.h
+//  evernote-sdk-ios
+//
+//  Created by A1@YX on 5/19/14.
+//  Copyright (c) 2014 n/a. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Utilities : NSObject
+
++ (UIImage *)imageWithImage:(UIImage *)image scaledToSize:(CGSize)newSize;
+@end
