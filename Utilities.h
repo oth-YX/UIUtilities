ee8364d9f5c36a3da353ccef5e635fc3f3b70660
oth-YX <oth-YX@users.noreply.github.com>
2014-05-19 15:42:29 +0300

    Update Utilities.h

--- Utilities.h
+++ Utilities.h
@@ -1,9 +1,9 @@
 //
 //  Utilities.h
-//  evernote-sdk-ios
+//  
 //
-//  Created by A1@YX on 5/19/14.
-//  Copyright (c) 2014 n/a. All rights reserved.
+//  Created by oth on 5/19/14.
+//
 //
 
 #import <Foundation/Foundation.h>
@@ -11,4 +11,5 @@
 @interface Utilities : NSObject
 
 + (UIImage *)imageWithImage:(UIImage *)image scaledToSize:(CGSize)newSize;
+
 @end
